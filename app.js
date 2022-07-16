e1c8f8d647c33719bff4e6da52bfe14da1482406
froggal <doeon111@naver.com>
2022-07-16 15:15:53 +0900

    end

---
+++ app.js
@@ -0,0 +1,4 @@
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i)=>{
+    a.src = 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjAyMTBfOTIg%2FMDAxNjQ0NDg1MzUxMTIx.Str-x-QLNHpmsWb_mU41o3U28twB6cNEgQ2hOlFdGEgg.K3rEFylOrNQCgDdAVweCbDUYVhjMEiCkJ8s-EO8ZIUEg.JPEG.rain_de_lune%2F1644485348868.jpg&type=a340'
+})
